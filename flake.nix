ad70202d8acf52de4b18eafecec308a24bbd3446
storvik <petterstorvik@gmail.com>
2023-04-15 21:44:38 +0200

    Add goshrt and goshrtc package to flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
     nix2container.url = "github:nlewo/nix2container";
 
     gitignore = {
@@ -12,70 +14,110 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = inputs@{ self, nixpkgs, gitignore, nix2container }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, gitignore, nix2container, gomod2nix }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      nix2containerPkgs = nix2container.packages.x86_64-linux;
-      inherit (gitignore.lib) gitignoreSource;
+      systems = [ "x86_64-linux" "aarch64" ];
     in
-    rec {
+    flake-utils.lib.eachSystem systems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ gomod2nix.overlays.default ];
+        };
+        nix2containerPkgs = nix2container.packages.x86_64-linux;
+        inherit (gitignore.lib) gitignoreSource;
+        pversion = "0.0.1";
+      in
+      rec {
 
-      # packages."x86_64-linux".goshrt-dev-container = nix2containerPkgs.nix2container.buildImage {
-      #   name = "goshrt-dev-container";
-      #   config = {
-      #     Cmd = [ "postgres" ];
-      #     WorkingDir = "/data";
-      #   };
-      # };
+        packages = {
+          goshrt = pkgs.buildGoApplication {
+            pname = "goshrt";
+            version = pversion;
+            pwd = ./.;
+            src = ./.;
+            subPackages = [ "cmd/goshrt" ];
+            modules = ./gomod2nix.toml;
+            ldflags = ''
+              -w -s -X 'github.com/storvik/goshrt/version.GitVersion=${pversion}'
+                    -X 'github.com/storvik/goshrt/version.GitCommit=${self.shortRev or "dirty"}'
+            '';
+            doCheck = false;
+          };
 
-      devShell."x86_64-linux" =
-        let
-          tmp = ".devshell";
-          db = "goshrt";
-          user = "goshrt";
-          passwd = "trhsog";
-          port = "6000";
-          pgdata = ".devshell/db";
-        in
-        pkgs.mkShell {
-          PGDATA = pgdata;
-          buildInputs = [
-            (pkgs.writeScriptBin "pgnix-init" ''
-              initdb -D ${pgdata} -U postgres
-              pg_ctl -D ${pgdata} -l ${pgdata}/postgres.log  -o "-p ${port} -k /tmp -i" start
-              createdb --port=${port} --host=localhost --username=postgres -O postgres ${db}
-              psql -d postgres -U postgres -h localhost -p ${port} -c "create user ${user} with encrypted password '${passwd}';"
-              psql -d postgres -U postgres -h localhost -p ${port} -c "grant all privileges on database ${db} to ${user};"
-            '')
-            (pkgs.writeScriptBin "pgnix-start" ''
-              pg_ctl -D ${pgdata} -l ${pgdata}/postgres.log  -o "-p ${port} -k /tmp -i" start
-            '')
-            (pkgs.writeScriptBin "pgnix-pgcli" ''
-              PGPASSWORD=${passwd} pgcli -h localhost -p 6000 -U goshrt
-            '')
-            (pkgs.writeScriptBin "pgnix-psql" ''
-              PGPASSWORD=${passwd} psql -d ${db} -U ${user} -h localhost -p ${port}
-            '')
-            (pkgs.writeScriptBin "pgnix-status" ''
-              pg_ctl -D ${pgdata} status
-            '')
-            (pkgs.writeScriptBin "pgnix-restart" ''
-              pg_ctl -D ${pgdata} restart
-            '')
-            (pkgs.writeScriptBin "pgnix-stop" ''
-              pg_ctl -D ${pgdata} stop
-            '')
-            (pkgs.writeScriptBin "pgnix-purge" ''
-              pg_ctl -D ${pgdata} stop
-              rm -rf .devshell/db
-            '')
-            pkgs.glibcLocales
-            pkgs.postgresql
-            pkgs.pgcli
-          ];
+          goshrtc = pkgs.buildGoApplication {
+            pname = "goshrtc";
+            version = pversion;
+            pwd = ./.;
+            src = ./.;
+            subPackages = [ "cmd/goshrtc" ];
+            modules = ./gomod2nix.toml;
+            ldflags = ''
+              -w -s -X 'github.com/storvik/goshrt/version.GitVersion=${pversion}'
+                    -X 'github.com/storvik/goshrt/version.GitCommit=${self.shortRev or "dirty"}'
+            '';
+            doCheck = false;
+          };
+
+          defaultPackage = packages.goshrt;
         };
 
-    };
+        devShell =
+          let
+            tmp = ".devshell";
+
+            db = "goshrt";
+            user = "goshrt";
+            passwd = "trhsog";
+            port = "6000";
+            pgdata = ".devshell/db";
+          in
+          pkgs.mkShell {
+            PGDATA = pgdata;
+
+            buildInputs = [
+              (pkgs.mkGoEnv { pwd = ./.; })
+              pkgs.glibcLocales
+              pkgs.postgresql
+              pkgs.pgcli
+              pkgs.gomod2nix
+              (pkgs.writeScriptBin "pgnix-init" ''
+                initdb -D ${pgdata} -U postgres
+                pg_ctl -D ${pgdata} -l ${pgdata}/postgres.log  -o "-p ${port} -k /tmp -i" start
+                createdb --port=${port} --host=localhost --username=postgres -O postgres ${db}
+                psql -d postgres -U postgres -h localhost -p ${port} -c "create user ${user} with encrypted password '${passwd}';"
+                psql -d postgres -U postgres -h localhost -p ${port} -c "grant all privileges on database ${db} to ${user};"
+              '')
+              (pkgs.writeScriptBin "pgnix-start" ''
+                pg_ctl -D ${pgdata} -l ${pgdata}/postgres.log  -o "-p ${port} -k /tmp -i" start
+              '')
+              (pkgs.writeScriptBin "pgnix-pgcli" ''
+                PGPASSWORD=${passwd} pgcli -h localhost -p 6000 -U goshrt
+              '')
+              (pkgs.writeScriptBin "pgnix-psql" ''
+                PGPASSWORD=${passwd} psql -d ${db} -U ${user} -h localhost -p ${port}
+              '')
+              (pkgs.writeScriptBin "pgnix-status" ''
+                pg_ctl -D ${pgdata} status
+              '')
+              (pkgs.writeScriptBin "pgnix-restart" ''
+                pg_ctl -D ${pgdata} restart
+              '')
+              (pkgs.writeScriptBin "pgnix-stop" ''
+                pg_ctl -D ${pgdata} stop
+              '')
+              (pkgs.writeScriptBin "pgnix-purge" ''
+                pg_ctl -D ${pgdata} stop
+                rm -rf .devshell/db
+              '')
+            ];
+          };
+      });
 }
