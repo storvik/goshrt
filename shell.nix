6ec284e202ce421b4f06208013044f22bf29739b
storvik <petterstorvik@gmail.com>
2023-04-20 15:23:07 +0200

    Move pgnix to ./nix/

--- shell.nix
+++ shell.nix
@@ -5,5 +5,5 @@ pkgs.mkShell
   buildInputs = [
     (pkgs.mkGoEnv { pwd = ./.; })
     pkgs.gomod2nix
-  ] ++ (import ./pgnix.nix { inherit pkgs; });
+  ] ++ (import ./nix/pgnix.nix { inherit pkgs; });
 }
