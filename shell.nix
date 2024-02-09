4dd81bfc25e7fed8b7c911bedecd5d2d64e61257
Petter S. Storvik <petterstorvik@gmail.com>
2024-02-09 11:46:01 +0100

    Fix consistent go version in nix / CI (#20)

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@
 pkgs.mkShell
 {
   buildInputs = [
-    (pkgs.mkGoEnv { pwd = ./.; })
+    # disabled as it does not pickup go version from go.mod
+    # (pkgs.mkGoEnv { pwd = ./.; })
+    pkgs.go_1_21
     pkgs.gomod2nix
     pkgs.govulncheck
     pkgs.golangci-lint
